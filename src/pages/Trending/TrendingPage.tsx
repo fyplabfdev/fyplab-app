b9a9ffb74f253fedffa109699b5e10cab9fe471e
FYPLAB <fyplabf@gmail.com>
2026-01-20 14:36:23 +0700

    Create TrendingPage.tsx

---
+++ src/pages/Trending/TrendingPage.tsx
@@ -0,0 +1,8 @@
+export function TrendingPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-semibold">Trending</h1>
+      <p className="text-sm opacity-70 mt-2">Trending ranking goes here.</p>
+    </div>
+  );
+}

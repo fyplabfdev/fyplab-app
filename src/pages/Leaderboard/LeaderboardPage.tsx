4f71e77efa5cf5edd07f3f2ee10370f69cb14f65
FYPLAB <fyplabf@gmail.com>
2026-01-20 14:36:51 +0700

    Create LeaderboardPage.tsx

---
+++ src/pages/Leaderboard/LeaderboardPage.tsx
@@ -0,0 +1,8 @@
+export function LeaderboardPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-semibold">Leaderboard</h1>
+      <p className="text-sm opacity-70 mt-2">Top predictors go here.</p>
+    </div>
+  );
+}

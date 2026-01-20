84365d9dd4c91663851aafd3bf0fa4c1c9881da3
FYPLAB <fyplabf@gmail.com>
2026-01-20 14:33:46 +0700

    Create providers.tsx

---
+++ src/app/providers.tsx
@@ -0,0 +1,7 @@
+import { PropsWithChildren } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "../lib/queryClient";
+
+export function Providers({ children }: PropsWithChildren) {
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+}
